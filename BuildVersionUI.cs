bf2e561890f29b4f02f5ae9b8d0faddb2bc08bd0
tsny <tsny700@gmail.com>
2018-09-27 22:41:29 -0500

    Initial Commit

---
+++ BuildVersionUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class BuildVersionUI : MonoBehaviour
+{
+    private Text text;
+
+    protected void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    protected void Start()
+    {
+        text.text = "Fetching build info...";
+        StartCoroutine(VersionChecker.GetVersions("https://itch.io/api/1/x/wharf/latest?target=tsny/project-lonestar&channel_name=win", this));
+    }
+
+    public void SetText(string liveVersion)
+    {
+        text.text = "Local version: " + Application.version;
+        text.text += "\nLive version: " + liveVersion;
+        text.text += "\nLonestar " + DateTime.Today.ToShortDateString();
+        text.text += "\nTaylor Snyder";
+    }
+}
---
+++ VersionChecker.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class VersionChecker : ScriptableObject
+{
+    public static IEnumerator GetVersions(string url, MonoBehaviour caller = null, bool printToConsole = false)
+    {
+        // Example URL: "https://itch.io/api/1/x/wharf/latest?target=tsny/project-lonestar&channel_name=win"
+
+        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log("ERROR: URL is either invalid or the network is down...");
+                yield break;
+            }
+
+            else
+            {
+                var json = JsonUtility.FromJson<ButlerInfo>(www.downloadHandler.text);
+
+                if (printToConsole)
+                {
+                    Debug.Log("Local version: " + Application.version);
+                    Debug.Log("Live version: " + json.latest);
+                }
+
+                if (caller != null)
+                {
+                    var buildUI = caller.GetComponent<BuildVersionUI>();
+
+                    if (buildUI != null)
+                        buildUI.SetText(json.latest);
+                }
+            }
+        }
+    }
+
+    public class ButlerInfo
+    {
+        public string latest;
+    }
+}
